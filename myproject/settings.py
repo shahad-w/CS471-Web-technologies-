150035696583704719d6dfbd6b087c514c220fa2
shahad <shahad.n.alwatban@gmail.com>
2024-10-13 07:28:53 +0300

    add ignore file

--- myproject/settings.py
+++ myproject/settings.py
@@ -27,10 +27,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
+    
+    'apps.shahadapp',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
